426655863628c089473634bf1142d4e26a2bd58b
Adam McGovern <adamsdropbox@gmail.com>
2015-08-28 15:34:54 -0500

    Proof of concept

--- scripts/main.js
+++ scripts/main.js
@@ -2,61 +2,31 @@
 //   Get the number of days until a date
 //
 // Dependencies:
-//   "<module name>": "<module version>"
+//   "<moment>": "<2.10.6>"
 //
 // Configuration:
 //   LIST_OF_ENV_VARS_TO_SET
 //
 // Commands:
-//   hubot <trigger> - <what the respond trigger does>
-//   <trigger> - <what the hear trigger does>
+//   hubot <how many days until MM/dd/yyyy> - <X days until MM/dd/yyyy>
 //
 // Notes:
 //   <optional notes required for the script>
 //
 // Author:
-//   <github username of the original script author>
+//   <adamsdropbox>
 var moment = require('moment');
 module.exports = function (robot) {
-  ////
-  // EXAMPLES
-  // Below are some example Hubot interactions. You will want to delete these
-  // from your actual script.
-  ////
 
-  // The 'hear' callback will match against any chat in text, not just messages
-  // directed at Hubot.
-  robot.hear(/orly/i, function (res) {
-    // `send` will simply post this message back to chat
-    res.send('> '+ moment());
+  robot.respond(/how many days until (.+)(\?*)/i, function (res) {
+    var now = moment();
+    var query = moment(res.match[1]);
+    if (query.isValid()){
+      var duration = now.to(query, true);
+      res.reply(duration + ' until '+ query.format('MMM DD, YYYY'));
+    } else {
+      res.reply('Are you sure that\'s a real date?');
+    }
   });
 
-  // The `respond` callback will only match if Hubot is mentioned at the start
-  // of the message. E.g.,
-  //
-  //   you: hubot speak
-  //   you: @hubot speak
-  robot.respond(/speak/i, function (res) {
-    // `reply` will, naturally, reply to the original sender.
-    //
-    //   you: hubot speak
-    //   hubot: @you Arf!
-    res.reply('Arf!');
-
-    // `emote` will "emote" the text instead of saying it. In HipChat, for
-    // example, this uses the /me command.
-    //
-    //   you: hubot speak
-    //   hubot wags its tail
-    res.emote('wags its tail');
-  });
-
-  // Regex match groups are available on the Response object. The format is
-  // identical to any other JavaScript regex match, so the first element of the
-  // array is the full text and subsequent elements are the capture groups in
-  // the order they appear.
-  robot.respond(/shout (.*)/i, function (res) {
-    var msg = res.match[1];
-    res.send(msg.toUpperCase());
-  });
 };
