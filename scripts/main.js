82363501014d300ca1f00223ecebf733fbab0a85
Adam McGovern <adamsdropbox@gmail.com>
2015-09-23 15:43:35 -0500

    resolve #1 - Add brain support

--- scripts/main.js
+++ scripts/main.js
@@ -19,30 +19,58 @@
 var moment = require('moment');
 module.exports = function (robot) {
 
+  function namespace(key){
+    return 'hubot-time-until-'+ key;
+  }
+
   var dateFormat = 'MM/DD/YYYY';
 
-  var customEvents = {
-    'adams wedding': moment('09/05/2015', dateFormat),
-    'slalom holiday party': moment('12/12/2015', dateFormat)
-  };
+  var customEvents = robot.brain.get(namespace('events')) || {};
 
-  robot.respond(/how long until (.+)/i, function (res) {
+  // Query a date
+  robot.respond(/how long until ([0-1][0-9][\-|\/][0-3][0-9][\-|\/][0-9]{4})[\?|\s]*(--save \w+)*/i, function (res) {
     var query = res.match[1];
+    var save = res.match[2];
 
     var date = moment(query, dateFormat);
-    if (customEvents[query.toLowerCase()] != undefined){
-      date = customEvents[query.toLowerCase()];
-    }
+
+    // Custom Event?
+    res.reply('q: '+ query +'; s: '+ save);
     if (date.isValid()){
       var duration = date.toNow(true);
       res.reply(duration + ' until '+ date.format('MMM DD, YYYY'));
+
+      // Save Event?
+      var saveName = save.split('--save ')[1];
+      if (saveName){
+        customEvents[saveName.toLowerCase()] = date;
+        robot.brain.set(namespace('events'), customEvents);
+      }
     } else {
       res.reply('Are you sure that\'s a real date?');
     }
   });
 
+  // Query an event by name
+  robot.respond(/how long until ([a-z]+)+\?*/i, function (res) {
+    var query = res.match[1];
+
+    if (customEvents[query.toLowerCase()] != undefined){
+      var date = customEvents[query.toLowerCase()];
+      var duration = date.toNow(true);
+      res.reply(duration + ' until '+ date.format('MMM DD, YYYY'));
+    } else {
+      res.reply('Custom event not found.');
+    }
+  });
+
   robot.respond(/show custom dates/i, function (res) {
-    res.reply(JSON.stringify(customEvents));
+    var events = robot.brain.get(namespace('events'));
+    if (events){
+      res.reply(JSON.stringify(events));
+    } else {
+      res.reply('No custom events saved. Add one with "hubot how long until <'+ dateFormat +'> --save <event name>"');
+    }
   });
 
 };
